f5321a4367c4884f7a684ba75725adb68172f077
jflute <dbflute@gmail.com>
2017-10-20 04:14:23 +0900

    avoid failure of todo "LastaDoc, support StreamResponse's produce"

--- src/main/java/org/lastaflute/doc/SwaggerGenerator.java
+++ src/main/java/org/lastaflute/doc/SwaggerGenerator.java
@@ -83,7 +83,7 @@ public class SwaggerGenerator {
     /* e.g. SwaggerAction implementation
     @AllowAnyoneAccess
     public class SwaggerAction extends FortressBaseAction {
-
+    
         // ===================================================================================
         //                                                                           Attribute
         //                                                                           =========
@@ -91,7 +91,7 @@ public class SwaggerGenerator {
         private RequestManager requestManager;
         @Resource
         private FortressConfig config;
-
+    
         // ===================================================================================
         //                                                                             Execute
         //                                                                             =======
@@ -101,13 +101,13 @@ public class SwaggerGenerator {
             String swaggerJsonUrl = toActionUrl(SwaggerAction.class, moreUrl("json"));
             return new SwaggerAgent(requestManager).prepareSwaggerUiResponse(swaggerJsonUrl);
         }
-
+    
         @Execute
         public JsonResponse<Map<String, Object>> json() {
             verifySwaggerAllowed();
             return asJson(new SwaggerGenerator().generateSwaggerMap());
         }
-
+    
         private void verifySwaggerAllowed() { // also check in ActionAdjustmentProvider
             verifyOrClientError("Swagger is not enabled.", config.isSwaggerEnabled());
         }
@@ -116,16 +116,16 @@ public class SwaggerGenerator {
 
     /* e.g. LastaDocTest implementation
     public class ShowbaseLastaDocTest extends UnitShowbaseTestCase {
-
+    
         @Override
         protected String prepareMockContextPath() {
             return ShowbaseBoot.CONTEXT; // basically for swagger
         }
-
+    
         public void test_document() throws Exception {
             saveLastaDocMeta();
         }
-
+    
         public void test_swaggerJson() throws Exception {
             saveSwaggerMeta(new SwaggerAction());
         }
@@ -506,8 +506,14 @@ public class SwaggerGenerator {
         produceMap.put(JsonResponse.class, Arrays.asList("application/json"));
         produceMap.put(XmlResponse.class, Arrays.asList("application/xml"));
         produceMap.put(HtmlResponse.class, Arrays.asList("text/html"));
+        // TODO you LastaDoc, support StreamResponse's produce by jflute
         //produceMap.put(StreamResponse.class, "");
-        return OptionalThing.of(produceMap.get(actiondocMeta.getReturnTypeDocMeta().getType()));
+        Class<?> produceType = actiondocMeta.getReturnTypeDocMeta().getType();
+        List<String> produceList = produceMap.get(produceType);
+        return OptionalThing.ofNullable(produceList, () -> {
+            String msg = "Not found the produce: type=" + produceType + ", keys=" + produceMap.keySet();
+            throw new IllegalStateException(msg);
+        });
     }
 
     protected Map<Class<?>, Tuple3<String, String, Function<Object, Object>>> createTypeMap() {
